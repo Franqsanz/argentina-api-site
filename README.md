cb658c0a9f75ed12b9057d0ec5b2db722ccdcc62
franco sanchez <franco_genta@hotmail.com>
2020-07-22 00:11:21 -0300

    clear index.js

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
+![netlify badge](https://app.netlify.com/sites/Franqsanz/deploys)
+
 # Argentina API Site PWA
 ![Argentina API](/icons/arg_api.png)
 
 
 > Si encuentras algún error o crees que algo esta mal, no dudes en escribir un [inssue](https://github.com/Franqsanz/argentina-api/issues). 😊
 
-Construido en puro HTML, CSS y JS.
+Construido con puro HTML, CSS y JS.
 
 [Sitio web.](https://argentina-api.netlify.app/)
 
--- js/index.js
+++ js/index.js
@@ -15,93 +15,15 @@ async function registerSW() {
     }
 }
 
-// const provinces = []
-
-// fetch('http://localhost:33528/api/v1/provinces')
-//     .then(res => res.json())
-//     .then(data => provinces.push(...data))
-
-// function findProvinces(a, province) {
-//     return province.filter(place => {
-//         const regex = new RegExp(a, 'gi')
-//         return place.province.match(regex)
-//     })
-// }
-
 function numberCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-// function displayProvince() {
-//     const provinceArray = findProvinces(this.value, provinces)
-//     const htmlProvince = provinceArray.map(province => {
-//         const regex = new RegExp(this.value, 'gi')
-//         const provinceName = province.province.replace(regex, `<mark class="markName">${this.value}</mark>`)
-//         return `
-//             <div class="card">
-//                 <div class="infoCity"> ${provinceName} </div>
-//                 <div class="infoPopulation">
-//                     <h2>Población</h2>
-//                     ${numberCommas(province.populations)}
-//                 </div>
-//                 <div class="infoProvince">
-//                     <h2>Ciudad Capital</h2>
-//                     ${province.capital}
-//                 </div>
-//                 <div class="infoProvince">
-//                     <h2>Fundación</h2>
-//                     ${numberCommas(province.foundation)}
-//                 </div>
-//                 <div class="infoProvince">
-//                 <h2>Fundador</h2>
-//                     ${numberCommas(province.founder || 'Desconocido')}
-//                 </div>
-//                 <div class="infoProvince">
-//                     <h2>Coordenadas</h2>
-//                     <ul class="ulCoord">
-//                         <li class="liCoord">
-//                             <h2>Latitud:</h2> ${province.latitude}
-//                         </li>
-//                         <li class="liCoord">
-//                             <h2>Longitud:</h2> ${province.longitude}
-//                         </li>
-//                     </ul>
-//                 </div>
-//                 <div class="infoRank">
-//                     <h2>Rango</h2>
-//                     ${province.rank}
-//                 </div>
-//             </div>
-//             `
-//     }).join('');
-//     section.innerHTML = htmlProvince
-
-//     if (!htmlProvince) {
-//         const notData = `
-//             <div class="notData">
-//                 🧐¡Ups esta provincia no existe!
-//             </div>
-//         `
-//         section.innerHTML = notData
-//     }
-// }
-
-// const search = document.querySelector('.search')
 const cards = document.querySelector('.contentCards')
-// const contentSearch = document.querySelector('.contentSearch')
-
-// search.addEventListener('change', displayProvince)
-// search.addEventListener('keyup', displayProvince)
 
 async function callApi() {
     let load = '<div class="load"></div>'
     cards.innerHTML = load
-
-    // let filter = `
-    //         <div class="filterSearch" id="provincias">
-    //             <input type="text" class="search" placeholder="Buscar Provincias">
-    //         </div>`
-    // contentSearch.innerHTML = filter
 
     let endpoint = 'http://localhost:33528/api/v1/provinces/';
     const res = await fetch(endpoint);
@@ -114,42 +36,42 @@ async function callApi() {
             <div class="card fontOperator">
                 <div class="infoCity"> ${province.province} </div>
                 <div class="infoPopulation">
-                    <h2>Población</h2>
+                    <h2 class="titleContentCard">Población</h2>
                     ${numberCommas(province.populations)}
                 </div>
                 <div class="infoProvince">
-                    <h2>Capital</h2>
+                    <h2 class="titleContentCard">Capital</h2>
                     ${province.capital}
                 </div>
                 <div class="infoProvince">
-                <h2>Superficie Km²</h2>
+                    <h2 class="titleContentCard">Superficie Km²</h2>
                     ${numberCommas(province.surface)}
                 </div>
                 <div class="infoProvince">
-                <h2>Fundación</h2>
+                    <h2 class="titleContentCard">Fundación</h2>
                     ${numberCommas(province.foundation)}
                 </div>
                 <div class="infoProvince">
-                <h2>Fundador</h2>
+                    <h2 class="titleContentCard">Fundador</h2>
                     ${numberCommas(province.founder || 'Desconocido')}
                 </div>
                 <div class="infoProvince">
-                    <h2>Coordenadas</h2>
+                    <h2 class="titleContentCard">Coordenadas</h2>
                     <ul class="ulCoord">
                         <li class="liCoord">
-                            <h2>Latitud:</h2> ${province.latitude}
+                            <h2 class="titleContentCard">Latitud:</h2> ${province.latitude}
                         </li>
                         <li class="liCoord">
-                            <h2>Longitud:</h2> ${province.longitude}
+                            <h2 class="titleContentCard">Longitud:</h2> ${province.longitude}
                         </li>
                     </ul>
                 </div>
                 <div class="infoRank">
-                    <h2>Región Geográfica</h2>
+                    <h2 class="titleContentCard">Región Geográfica</h2>
                     ${province.geographic_region}
                 </div>
                 <div class="infoRank">
-                    <h2>Rango</h2>
+                    <h2 class="titleContentCard">Rango</h2>
                     ${province.rank}
                 </div>
             </div>
