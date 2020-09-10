c4d9bca36a954b992bde92a465292920e4dbda11
franco sanchez <franco_genta@hotmail.com>
2020-09-10 11:54:02 -0300

    update btn menu 'aria'

--- js/event.js
+++ js/event.js
@@ -7,6 +7,16 @@ const aNav = document.querySelectorAll('.aNav');
 function menuOpen() {
     btnFloat.classList.toggle('open');
     nav.classList.toggle('navOpen');
+
+    const status = nav.setAttribute('status', 'visible')
+
+    if (status === 'visible') {
+        nav.setAttribute('status', 'hidden')
+        btnFloat.setAttribute('aria-expanded', 'false');
+    } else {
+        nav.setAttribute('status', 'visible')
+        btnFloat.setAttribute('aria-expanded', 'true');
+    }
 }
 
 btnFloat.addEventListener('click', menuOpen);
